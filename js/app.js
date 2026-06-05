c9855afedb1b5e5932955226338ae8bf879be2d8
patrimoniumadvisorsxxi-dotcom <patrimoniumadvisorsxxi@gmail.com>
2026-06-05 11:01:11 -0600

    style: cambiar mapa a modo claro (CartoDB Voyager) para mejor legibilidad

--- js/app.js
+++ js/app.js
@@ -1344,7 +1344,7 @@ function initAgMap(existingLat, existingLng) {
   mapDiv._leafletMap = map;
 
   // Dark-themed tile layer (CartoDB Dark Matter)
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 19
@@ -1509,7 +1509,7 @@ function initDetailMap(canvasId, lat, lng) {
     touchZoom: false,
   });
 
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 19
